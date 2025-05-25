7f9d83af239094f0ba427348c54e46259814b73d
jiebao <77776338+wangyujie615@users.noreply.github.com>
2025-05-25 22:48:49 +0800

    创建bem规范

---
+++ packages/components/icon/src/icon.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { createNameSpace } from '@wyj-ui/utils/create'
+const bem = createNameSpace('icon')
+//计算属性计算一个样式
+</script>
+<template>
+    <i>
+        <slot></slot>
+    </i>
+</template>
--- packages/utils/create.ts
+++ packages/utils/create.ts
@@ -1,1 +1,38 @@
-//BEM规范 block element modifier state
+// BEM规范 block element modifier state
+// 
+export function createNameSpace(prefix: string) {
+    const prefixName = `wyj-${prefix}`
+    return createBEM(prefixName)
+}
+function _bem(prefix: string, block: string, element: string, modifier: string) {
+    if (block) {
+        prefix += `-${block}`
+    }
+    if (element) {
+        prefix += `__${element}`
+    }
+    if (modifier) {
+        prefix += `--${modifier}`
+    }
+    return prefix
+}
+function createBEM(prefix: string) {
+    const b = (block: string = '') => _bem(prefix, block, "", "")
+    const e = (element: string = '') => element ? _bem(prefix, "", element, "") : ''
+    const m = (modifier: string = '') => modifier ? _bem(prefix, "", "", modifier) : ''
+    const be = (block: string = '', element: string = '') => (block && element) ? _bem(prefix, block, element, "") : ''
+    const bm = (block: string = '', modifier: string = '') => (block && modifier) ? _bem(prefix, block, "", modifier) : ''
+    const em = (element: string = '', modifier: string = '') => (element && modifier) ? _bem(prefix, "", element, modifier) : ''
+    const bem = (block: string = '', element: string = '', modifier: string = '') => (block && element && modifier) ? _bem(prefix, block, element, modifier) : ''
+    const is = (name: string, state: boolean) => state ? `$is-{name}` : ''
+    return {
+        b,
+        e,
+        m,
+        be,
+        bm,
+        em,
+        bem,
+        is
+    }
+}
