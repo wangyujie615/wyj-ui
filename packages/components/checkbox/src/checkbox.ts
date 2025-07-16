f43f5f0faf3d26a4c6fb05fe8a34d517fa4e563b
jiebao <77776338+wangyujie615@users.noreply.github.com>
2025-07-17 01:41:33 +0800

    Update checkbox.ts

--- packages/components/checkbox/src/checkbox.ts
+++ packages/components/checkbox/src/checkbox.ts
@@ -1,20 +1,20 @@
 import { ExtractPropTypes, PropType } from "vue";
 
 export const checkboxProps = {
-  modelValue: { 
-    type: [Boolean,String,Number] as PropType<boolean | string | number>, 
-    default: false 
+  modelValue: {
+    type: [Boolean, String, Number] as PropType<boolean | string | number>,
+    default: false
   },
   indeterminate: Boolean,
   disabled: Boolean,
-  label:{
+  label: {
     type: String as PropType<string>,
   }
 } as const
 export type CheckboxProps = Partial<ExtractPropTypes<typeof checkboxProps>>
 
 export const checkboxEmits = {
-  'update:modelValue': (value: boolean | string | number)=> value,
-  'change':(value: boolean | string | number ) => value
+  'update:modelValue': (value: boolean | string | number) => value,
+  'change': (value: boolean | string | number) => value
 }
 export type CheckboxEmits = typeof checkboxEmits
