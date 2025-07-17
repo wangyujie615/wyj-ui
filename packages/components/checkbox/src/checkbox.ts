b7d275d6fb10d03ca62c67582abbc27ecedd0375
jiebao <77776338+wangyujie615@users.noreply.github.com>
2025-07-18 00:45:41 +0800

    checkbox功能完善

--- packages/components/checkbox/src/checkbox.ts
+++ packages/components/checkbox/src/checkbox.ts
@@ -1,16 +1,21 @@
 import { ExtractPropTypes, PropType } from "vue";
-
+// 组件的Props
 export const checkboxProps = {
+  // 勾选框的数据
   modelValue: {
     type: [Boolean, String, Number] as PropType<boolean | string | number>,
     default: false
   },
+  // 默认选中
   indeterminate: Boolean,
+  // 是否禁用
   disabled: Boolean,
+  // 标签值
   label: {
     type: String as PropType<string>,
   }
 } as const
+
 export type CheckboxProps = Partial<ExtractPropTypes<typeof checkboxProps>>
 
 export const checkboxEmits = {
--- packages/components/checkbox/src/checkbox.vue
+++ packages/components/checkbox/src/checkbox.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
 import { createNameSpace } from '@wyj-ui/utils/create';
 import { checkboxEmits, checkboxProps } from './checkbox';
-import { computed } from 'vue';
+import { computed, onMounted, watch, ref } from 'vue';
 
+// 创建bem规范
 const bem = createNameSpace('checkbox')
+// 组件命名
 defineOptions({
   name: 'WCheckbox'
 })
+// 组件的props
 const props = defineProps(checkboxProps)
+// 组件的事件emits
 const emit = defineEmits(checkboxEmits)
-
+// 计算属性
 const model = computed<any>({
   get() {
     return props.modelValue
@@ -18,14 +22,41 @@ const model = computed<any>({
     emit('update:modelValue', value)
   }
 })
+
+// ------------ 操作类 -------------
+// 监听传入的indeterminate状态 执行函数
+watch(() => props.indeterminate, indeterminate)
+
+// 挂载时,执行函数
+onMounted(() => {
+  indeterminate(props.indeterminate)
+})
+
+// ----------- 操作组件 ------------
+const inputRef = ref<HTMLInputElement>()
+
+// ----------- 方法区 --------------
+
+function indeterminate(value: boolean) {
+  inputRef.value!.indeterminate = value
+}
+
+function handleChange(e: Event) {
+  const target = e.target as HTMLInputElement
+  const value = target.checked ? true : false
+  emit('change', value)
+}
 </script>
 <template>
   <label :class="bem.b()">
+    <!-- 勾选框:显示勾选 -->
     <span :class="bem.e('input')">
-      <input type="checkbox" v-model="model" />
+      <input type="checkbox" v-model="model" ref="inputRef" :disabled="disabled" :value="label"
+        @chaneg="handleChange" />
     </span>
     <span v-if="$slots.default || label" :class="bem.e('label')">
       <slot></slot>
+      <!-- 插槽不存在显示标签 -->
       <template v-if="!$slots.default">
         {{ label }}
       </template>
